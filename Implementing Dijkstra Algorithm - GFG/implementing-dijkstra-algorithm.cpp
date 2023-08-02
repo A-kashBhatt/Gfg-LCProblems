00fb91eadfa860a8024d3942e6484ebd42f0eb63
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-08-02 18:25:24 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
 	public:
@@ -10,20 +11,22 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-       pq.push({0,S});
+       set<pair<int,int>>st;
+       st.insert({0,S});
        vector<int>distance(V,1e9);
        distance[S]=0;
-       while(!pq.empty()){
-           int node=pq.top().second;
-           int dis=pq.top().first;
-           pq.pop();
+       while(!st.empty()){
+           auto curr=*(st.begin());
+           int node=curr.second;
+           int dis=curr.first;
+           st.erase(curr);
            for(auto it:adj[node]){
                int adjNode=it[0];
                int edgeWeight=it[1];
                if(distance[adjNode]>dis+edgeWeight){
+                   if(distance[adjNode]!=1e9) st.erase({distance[adjNode],adjNode});
                    distance[adjNode]=dis+edgeWeight;
-                   pq.push({distance[adjNode],adjNode});
+                   st.insert({distance[adjNode],adjNode});
                }
            }
        }

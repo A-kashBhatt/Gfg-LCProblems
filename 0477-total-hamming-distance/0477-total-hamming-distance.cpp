6d6027014e0ecee6bfaa1f7791d9e63eb135d7ff
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-08-05 10:46:25 +0530

    Time: 15 ms (100.00%), Space: 19 MB (67.87%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        vector<int>ones(32,0);
-        int sz=nums.size();
+        // vector<int>ones(32,0);
+        int sz=nums.size(),ans=0;
         for(int i=0;i<32;i++){
             int mask=(1<<i);
+            int ones=0;
             for(int j=0;j<sz;j++){
-                if(nums[j]&mask)ones[i]++;
+                if(nums[j]&mask)ones++;
             }
-        }
-        int ans=0;
-        for(int i=0;i<32;i++){
-            int a=ones[i],b=sz-a;
+            int a=ones,b=sz-ones;
             ans+=(a*b);
         }
         return ans;

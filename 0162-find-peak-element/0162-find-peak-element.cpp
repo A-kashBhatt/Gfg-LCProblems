2ba69fddfe4cb8990f7769b5541a79a113f2430c
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-08-06 13:56:26 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (49.33%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-      if(nums.size()==1)return 0;
-      int s=0,e=nums.size()-1;
-       while(e>=s){
-           int m=s+(e-s)/2;
-           if(m==0){
-               return nums[0]>nums[1]?0:1;
-           }
-           if(m==nums.size()-1){
-               return nums[nums.size()-1]>nums[nums.size()-2]?nums.size()-1:nums.size()-2;
-           }
-           
-           if(nums[m]>nums[m+1] && nums[m]>nums[m-1])return m;
-           
-           else if(nums[m]<nums[m-1]){
-               e=m-1;
-           }
-           else s=m+1;    
-       }
-       return 1;
-      
+    int findPeakElement(vector<int>& arr) {
+        int s=0,sz=arr.size(),e=sz-1;
+        if(sz==1)return 0;
+        while(e>=s){
+            int mid=s+(e-s)/2;
+            if(mid==0) return arr[mid]>arr[mid+1]?0:1;
+            if(mid==sz-1) return arr[mid]>arr[mid-1]?mid:mid-1;
+            
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return mid;
+            else if(arr[mid]<arr[mid-1])e=mid-1;
+            else s=mid+1;
+        }
+        return -1;
     }
 };

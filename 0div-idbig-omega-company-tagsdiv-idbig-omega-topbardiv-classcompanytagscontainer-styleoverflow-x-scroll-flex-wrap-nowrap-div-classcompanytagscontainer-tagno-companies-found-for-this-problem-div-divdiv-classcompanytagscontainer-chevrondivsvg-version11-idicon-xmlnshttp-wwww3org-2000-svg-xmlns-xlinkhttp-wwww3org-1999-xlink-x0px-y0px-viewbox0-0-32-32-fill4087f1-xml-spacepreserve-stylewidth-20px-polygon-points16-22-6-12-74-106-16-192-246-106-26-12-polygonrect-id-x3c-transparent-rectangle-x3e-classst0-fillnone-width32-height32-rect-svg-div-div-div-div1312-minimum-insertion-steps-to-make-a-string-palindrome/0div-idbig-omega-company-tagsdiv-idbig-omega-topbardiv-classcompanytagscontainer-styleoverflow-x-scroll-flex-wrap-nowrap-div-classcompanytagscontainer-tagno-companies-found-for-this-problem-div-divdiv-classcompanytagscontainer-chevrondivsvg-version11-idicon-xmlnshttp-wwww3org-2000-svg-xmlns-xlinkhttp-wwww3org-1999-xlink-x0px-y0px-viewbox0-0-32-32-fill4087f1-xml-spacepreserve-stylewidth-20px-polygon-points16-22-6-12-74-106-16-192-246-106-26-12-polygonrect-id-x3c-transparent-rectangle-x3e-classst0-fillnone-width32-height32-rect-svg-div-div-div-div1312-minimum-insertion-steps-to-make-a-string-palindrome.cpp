d4d789b18354eb090ff7dffad2c4e8ef3cb84e6e
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-21 17:07:04 +0530

    Time: 45 ms (66.98%), Space: 7 MB (87.79%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,31 @@
+//recursion
+class Solution {
+    int lcs(string s, string t)
+{   
+	int sz1=s.size(),sz2=t.size(),x=0,prevVar=0,currVar=0;
+	vector<int>prev(sz2,0); 
+	for(int i=0;i<sz1;i++){
+		for(int j=0;j<sz2;j++){
+			currVar=prev[j];
+			if(s[i]==t[j]){
+				if(j-1>=0)prev[j]=1+prevVar;
+				else prev[j]=1;
+			}
+			else{
+				int a=0;
+				if(j-1>=0)a=prev[j-1];
+				prev[j]=max(a,prev[j]);
+			}
+			prevVar=currVar;
+		}
+		prevVar=0;
+	}
+	return prev[sz2-1];
+}
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return s.size()-lcs(s,t);
+    }
+};

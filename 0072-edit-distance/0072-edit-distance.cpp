f4023eb0f64f4bb73aa0363f3fc4ddbe0610993a
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-21 22:09:06 +0530

    Time: 7 ms (84.47%), Space: 6.7 MB (90.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,21 +2,23 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int sz1=word1.size(),sz2=word2.size();
-        vector<int>prev(sz2+1,0),curr(sz2+1,0);
+        int sz1=word1.size(),sz2=word2.size(),prevVal,currVal;
+        vector<int>prev(sz2+1,0);
         for(int i=0;i<=sz2;i++)prev[i]=i;
         for(int i=1;i<=sz1;i++){
-            curr[0]=i;
+            prevVal=prev[0];
+            prev[0]=i;
             for(int j=1;j<=sz2;j++){
-                if(word1[i-1]==word2[j-1])curr[j]=prev[j-1];
+                currVal=prev[j];
+                if(word1[i-1]==word2[j-1])prev[j]=prevVal;
                 else{
-                    int replace=1+prev[j-1];
-                    int insert=1+curr[j-1];
+                    int replace=1+prevVal;
+                    int insert=1+prev[j-1];
                     int remove=1+prev[j];
-                    curr[j]=min(replace,min(insert,remove));
+                    prev[j]=min(replace,min(insert,remove));
                 }
+                prevVal=currVal;
             }
-            prev=curr;
         }
         return prev[sz2];
     }

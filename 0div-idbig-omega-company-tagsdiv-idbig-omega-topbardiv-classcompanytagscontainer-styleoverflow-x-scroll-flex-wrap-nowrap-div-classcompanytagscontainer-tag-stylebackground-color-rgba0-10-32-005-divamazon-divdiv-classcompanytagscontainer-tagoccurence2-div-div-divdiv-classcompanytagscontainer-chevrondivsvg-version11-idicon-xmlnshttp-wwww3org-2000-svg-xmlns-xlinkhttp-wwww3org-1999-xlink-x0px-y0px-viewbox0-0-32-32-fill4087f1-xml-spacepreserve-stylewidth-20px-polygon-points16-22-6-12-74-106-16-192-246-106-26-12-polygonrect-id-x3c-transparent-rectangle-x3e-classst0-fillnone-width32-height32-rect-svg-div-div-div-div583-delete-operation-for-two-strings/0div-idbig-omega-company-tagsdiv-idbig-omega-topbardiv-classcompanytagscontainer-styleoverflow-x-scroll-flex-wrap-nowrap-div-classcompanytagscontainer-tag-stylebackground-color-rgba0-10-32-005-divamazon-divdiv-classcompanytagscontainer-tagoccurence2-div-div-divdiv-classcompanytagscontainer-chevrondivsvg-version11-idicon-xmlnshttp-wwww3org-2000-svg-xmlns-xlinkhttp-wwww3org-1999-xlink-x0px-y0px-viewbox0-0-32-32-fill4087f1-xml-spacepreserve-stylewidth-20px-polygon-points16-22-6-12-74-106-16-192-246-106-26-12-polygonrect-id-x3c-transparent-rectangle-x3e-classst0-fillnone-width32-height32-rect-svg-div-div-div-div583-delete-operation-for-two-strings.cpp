335b7b49c435a5d202f645e8581d898e46a96d76
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-21 17:25:39 +0530

    Time: 10 ms (89.79%), Space: 6.9 MB (90.11%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div583-delete-operation-for-two-strings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int longestCommonSubsequence(string s, string t) {
+        int sz1=s.size(),sz2=t.size(),x=0,prevVar=0,currVar=0;
+	vector<int>prev(sz2,0); 
+	for(int i=0;i<sz1;i++){
+		for(int j=0;j<sz2;j++){
+			currVar=prev[j];
+			if(s[i]==t[j]){
+				if(j-1>=0)prev[j]=1+prevVar;
+				else prev[j]=1;
+			}
+			else{
+				int a=0;
+				if(j-1>=0)a=prev[j-1];
+				prev[j]=max(a,prev[j]);
+			}
+			prevVar=currVar;
+		}
+		prevVar=0;
+	}
+	return prev[sz2-1];
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int lcs=longestCommonSubsequence(word1,word2),sz1=word1.size(),
+        sz2=word2.size();
+        return (sz1-lcs+sz2-lcs);
+    }
+};

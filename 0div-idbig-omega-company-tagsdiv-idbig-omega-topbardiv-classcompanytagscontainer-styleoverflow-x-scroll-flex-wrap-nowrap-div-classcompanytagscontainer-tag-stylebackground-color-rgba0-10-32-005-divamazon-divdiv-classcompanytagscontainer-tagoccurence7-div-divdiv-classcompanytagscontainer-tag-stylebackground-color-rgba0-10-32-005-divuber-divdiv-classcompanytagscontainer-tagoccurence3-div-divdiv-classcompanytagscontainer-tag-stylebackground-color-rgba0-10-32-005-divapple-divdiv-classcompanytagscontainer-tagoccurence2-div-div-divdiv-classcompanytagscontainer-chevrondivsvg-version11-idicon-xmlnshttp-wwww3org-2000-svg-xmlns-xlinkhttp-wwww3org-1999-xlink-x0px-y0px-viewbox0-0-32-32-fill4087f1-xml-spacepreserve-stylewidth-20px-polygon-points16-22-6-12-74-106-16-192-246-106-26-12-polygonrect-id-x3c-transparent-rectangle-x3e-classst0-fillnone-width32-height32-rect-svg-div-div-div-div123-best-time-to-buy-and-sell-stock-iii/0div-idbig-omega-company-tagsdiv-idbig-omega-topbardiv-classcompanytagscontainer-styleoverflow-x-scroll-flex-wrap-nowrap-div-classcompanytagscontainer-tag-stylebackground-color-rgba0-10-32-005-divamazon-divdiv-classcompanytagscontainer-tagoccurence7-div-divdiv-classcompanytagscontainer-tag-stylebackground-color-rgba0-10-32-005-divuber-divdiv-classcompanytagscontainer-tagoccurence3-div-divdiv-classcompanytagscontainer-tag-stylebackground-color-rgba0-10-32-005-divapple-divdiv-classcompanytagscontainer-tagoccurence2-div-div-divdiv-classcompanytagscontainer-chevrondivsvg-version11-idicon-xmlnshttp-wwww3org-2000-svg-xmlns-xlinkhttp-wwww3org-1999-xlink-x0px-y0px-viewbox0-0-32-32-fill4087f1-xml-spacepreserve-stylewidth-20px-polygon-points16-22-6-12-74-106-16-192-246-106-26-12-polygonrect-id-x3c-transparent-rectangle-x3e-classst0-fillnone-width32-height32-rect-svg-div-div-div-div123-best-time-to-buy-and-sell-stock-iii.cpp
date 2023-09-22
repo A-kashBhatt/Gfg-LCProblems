098455dd36228b3d153311aa62fbe362b68ade87
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-22 14:59:04 +0530

    Time: 552 ms (34.59%), Space: 207.3 MB (39.12%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,24 @@
-//memoization
+//tabulation
 class Solution {
-    int helper(vector<int>&prices,int ind,int buy,int n,int trades,
-              vector<vector<vector<int>>>&dp){
-            if(ind==n || trades==0)return 0;
-            if(dp[ind][buy][trades]!=-1)return dp[ind][buy][trades];
-            if(buy){
-              return dp[ind][buy][trades]=max(-prices[ind]+ helper(prices,ind+1,0,n,trades,dp),helper(prices,ind+1,1,n,trades,dp));
-            }
-            else{
-              return dp[ind][buy][trades]= max(prices[ind]+ helper(prices,ind+1,1,n,trades-1,dp), helper(prices,ind+1,0,n,trades,dp));
-            }
-    }
-
-
+    
 public:
     int maxProfit(vector<int>& prices) {
         int sz=prices.size();
-        vector<vector<vector<int>>>dp(sz,vector<vector<int>>(2,vector<int>(3,-1)));
-        return helper(prices,0,1,sz,2,dp);
+        vector<vector<vector<int>>>dp(sz+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int ind=sz-1;ind>=0;ind--){
+            for(int buy=1;buy>=0;buy--){
+                for(int trades=2;trades>0;trades--){
+                    if(buy){
+                       dp[ind][buy][trades]=max(-prices[ind]+ dp[ind+1][0][trades], dp[ind+1][1][trades]);
+            }
+                    
+                    else{
+                        dp[ind][buy][trades]= max(prices[ind]+ dp[ind+1][1][trades-1], dp[ind+1][0][trades]);
+            }
+                    }
+                }
+            }
+        
+        return dp[0][1][2];
     }
 };

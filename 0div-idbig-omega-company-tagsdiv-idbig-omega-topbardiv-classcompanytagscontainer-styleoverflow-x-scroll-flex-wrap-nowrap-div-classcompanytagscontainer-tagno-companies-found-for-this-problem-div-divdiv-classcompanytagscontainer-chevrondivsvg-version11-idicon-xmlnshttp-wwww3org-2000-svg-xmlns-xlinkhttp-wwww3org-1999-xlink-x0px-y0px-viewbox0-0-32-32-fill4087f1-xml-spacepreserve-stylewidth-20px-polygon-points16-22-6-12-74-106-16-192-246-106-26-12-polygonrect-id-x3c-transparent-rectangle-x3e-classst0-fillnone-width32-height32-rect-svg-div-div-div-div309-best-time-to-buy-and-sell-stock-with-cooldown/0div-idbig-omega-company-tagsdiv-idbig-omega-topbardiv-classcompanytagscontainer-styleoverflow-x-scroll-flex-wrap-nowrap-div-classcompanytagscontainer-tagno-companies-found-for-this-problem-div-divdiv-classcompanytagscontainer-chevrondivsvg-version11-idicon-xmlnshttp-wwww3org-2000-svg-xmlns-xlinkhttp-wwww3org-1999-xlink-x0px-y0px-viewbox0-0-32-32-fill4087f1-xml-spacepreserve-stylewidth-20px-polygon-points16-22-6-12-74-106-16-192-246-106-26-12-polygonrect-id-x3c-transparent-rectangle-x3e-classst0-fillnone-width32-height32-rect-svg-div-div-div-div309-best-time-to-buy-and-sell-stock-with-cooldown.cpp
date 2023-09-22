7d58750827beb379888fa5ea379bf7bfa69f411b
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-22 16:05:32 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (75.36%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,27 @@
 //0 ->buy 1->sell 2->cool down --------->repeat this cycle
 
-//tabulation
+//tabulation with space optimization
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int sz=prices.size();
-        vector<vector<int>>dp(sz+1,vector<int>(3,0));
+        vector<int>curr(3,0),ahead(3,0);
         for(int ind=sz-1;ind>=0;ind--){
             for(int token=0;token<3;token++){
               if(token==0){//buy
-               dp[ind][token]=max(-prices[ind]+dp[ind+1][(token+1)%3],
-                    dp[ind+1][token]);
+               curr[token]=max(-prices[ind]+ahead[(token+1)%3],
+                    ahead[token]);
               }  
               else if(token==1){
-                dp[ind][token]=max(prices[ind]+dp[ind+1][(token+1)%3],
-                    dp[ind+1][token]); 
+                curr[token]=max(prices[ind]+ahead[(token+1)%3],
+                    ahead[token]); 
               } 
               else{
-                dp[ind][token]=dp[ind+1][(token+1)%3];
+                curr[token]=ahead[(token+1)%3];
               }
             }
+            ahead=curr;
         }
-        return dp[0][0];
+        return ahead[0];
     }
 };

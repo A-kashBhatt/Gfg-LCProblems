d41596ea384ad5fdf112ff94454df0dba6e4672b
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-22 16:03:42 +0530

    Time: 4 ms (57.32%), Space: 12.2 MB (13.54%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,26 @@
 //0 ->buy 1->sell 2->cool down --------->repeat this cycle
+
+//tabulation
 class Solution {
-    int helper(vector<int>& prices,int ind,int sz,int token,vector<vector<int>>&dp){
-        if(ind==sz)return 0;
-        if(dp[ind][token]!=-1)return dp[ind][token];
-        if(token==0){//buy
-            return dp[ind][token]=max(-prices[ind]+ helper(prices,ind+1,sz,token+1,dp),
-                    helper(prices,ind+1,sz,token,dp));
-        }
-        else if(token==1){//sell
-             return dp[ind][token]=max(prices[ind]+helper(prices,ind+1,sz,token+1,dp),
-                    helper(prices,ind+1,sz,token,dp));
-        }
-        else{//cooldown peroid
-            return dp[ind][token]=helper(prices,ind+1,sz,0,dp);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int sz=prices.size();
-        vector<vector<int>>dp(sz,vector<int>(3,-1));
-        return helper(prices,0,sz,0,dp);
+        vector<vector<int>>dp(sz+1,vector<int>(3,0));
+        for(int ind=sz-1;ind>=0;ind--){
+            for(int token=0;token<3;token++){
+              if(token==0){//buy
+               dp[ind][token]=max(-prices[ind]+dp[ind+1][(token+1)%3],
+                    dp[ind+1][token]);
+              }  
+              else if(token==1){
+                dp[ind][token]=max(prices[ind]+dp[ind+1][(token+1)%3],
+                    dp[ind+1][token]); 
+              } 
+              else{
+                dp[ind][token]=dp[ind+1][(token+1)%3];
+              }
+            }
+        }
+        return dp[0][0];
     }
 };

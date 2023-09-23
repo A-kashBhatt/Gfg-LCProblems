1740566e3b36f93de34994059e20fa55f3e5825a
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-23 12:30:05 +0530

    Time: 954 ms (7.47%), Space: 292.5 MB (6.44%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -1,22 +1,33 @@
 //memoization 
 class Solution {
-    int helper(vector<int>& nums,int ind,int lastInd,vector<vector<int>>&dp){
-        if(ind<0)return 0;
-        if(dp[ind][lastInd]!=-1)return dp[ind][lastInd];
-        if(lastInd==nums.size()){
-            return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
-        }
-        if(nums[ind]<nums[lastInd]){
-            return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
-        }
-        else{
-            return dp[ind][lastInd]=helper(nums,ind-1,lastInd,dp);
-        }
-    }
+    // int helper(vector<int>& nums,int ind,int lastInd,vector<vector<int>>&dp){
+    //     if(ind<0)return 0;
+    //     if(dp[ind][lastInd]!=-1)return dp[ind][lastInd];
+    //     if(lastInd==nums.size()){
+    //         return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
+    //     }
+    //     if(nums[ind]<nums[lastInd]){
+    //         return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
+    //     }
+    //     else{
+    //         return dp[ind][lastInd]=helper(nums,ind-1,lastInd,dp);
+    //     }
+    // }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int sz=nums.size();
-        vector<vector<int>>dp(sz,vector<int>(sz+1,-1));
-        return helper(nums,sz-1,sz,dp);
+        int n=nums.size();
+         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    for(int i=1;i<=n;i++)dp[i][0]=1;
+    for(int ind=1;ind<=n;ind++){
+        for(int lastInd=1;lastInd<=n;lastInd++){
+           if(lastInd==n || nums[ind-1]<nums[lastInd]){
+            dp[ind][lastInd]= max(1+dp[ind-1][ind-1],dp[ind-1][lastInd]);
+           } 
+           else{
+            dp[ind][lastInd]=dp[ind-1][lastInd];
+           }
+        }
+    }
+    return dp[n][n];
     }
 };

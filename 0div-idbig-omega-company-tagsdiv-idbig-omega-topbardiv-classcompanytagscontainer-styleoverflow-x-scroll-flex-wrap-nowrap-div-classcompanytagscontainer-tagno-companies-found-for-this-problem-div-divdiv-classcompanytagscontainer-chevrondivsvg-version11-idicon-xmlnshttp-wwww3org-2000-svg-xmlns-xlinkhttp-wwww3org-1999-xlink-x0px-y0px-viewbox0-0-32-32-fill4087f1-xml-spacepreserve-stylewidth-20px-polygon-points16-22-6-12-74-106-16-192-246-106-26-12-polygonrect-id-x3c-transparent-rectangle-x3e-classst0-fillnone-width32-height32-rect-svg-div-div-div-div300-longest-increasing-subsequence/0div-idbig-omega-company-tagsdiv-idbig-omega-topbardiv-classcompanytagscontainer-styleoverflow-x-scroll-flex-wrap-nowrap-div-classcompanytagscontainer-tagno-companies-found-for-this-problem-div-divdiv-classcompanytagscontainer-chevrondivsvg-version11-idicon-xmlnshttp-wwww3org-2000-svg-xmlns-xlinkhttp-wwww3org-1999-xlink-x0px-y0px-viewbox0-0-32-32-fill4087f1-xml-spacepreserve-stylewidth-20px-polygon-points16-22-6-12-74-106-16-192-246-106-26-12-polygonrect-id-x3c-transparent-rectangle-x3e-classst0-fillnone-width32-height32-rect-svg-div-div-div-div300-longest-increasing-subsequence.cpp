a2e34bac9a8b954e6cf16a3095b48250241cc8c8
A-kashBhatt <99075734+A-kashBhatt@users.noreply.github.com>
2023-09-29 23:06:15 +0530

    Time: 7 ms (86.61%), Space: 10.9 MB (40.70%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -1,27 +1,15 @@
-//memoization 
 class Solution {
-    // int helper(vector<int>& nums,int ind,int lastInd,vector<vector<int>>&dp){
-    //     if(ind<0)return 0;
-    //     if(dp[ind][lastInd]!=-1)return dp[ind][lastInd];
-    //     if(lastInd==nums.size()){
-    //         return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
-    //     }
-    //     if(nums[ind]<nums[lastInd]){
-    //         return dp[ind][lastInd]=max(1+helper(nums,ind-1,ind,dp), helper(nums,ind-1,lastInd,dp));
-    //     }
-    //     else{
-    //         return dp[ind][lastInd]=helper(nums,ind-1,lastInd,dp);
-    //     }
-    // }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int sz=nums.size();
-        vector<int>dp(sz,1);
-        for(int i=1;i<sz;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j])dp[i]=max(dp[i],1+dp[j]);
+        vector<int>v;
+        v.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>v.back())v.push_back(nums[i]);
+            else{
+                int ind=lower_bound(v.begin(),v.end(),nums[i])-v.begin();
+                v[ind]=nums[i];
             }
         }
-        return *max_element(dp.begin(),dp.end());
+        return v.size();
     }
 };

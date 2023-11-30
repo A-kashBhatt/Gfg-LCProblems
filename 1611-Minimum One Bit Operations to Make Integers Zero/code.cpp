ac0bcc7c3e35c771cee6f8ab4a8c0c613d8ed80a
bhattShahab <aakashbhatt758@gmail.com>
2023-11-30 23:04:21 +0530

    Leetcode POTD 30-Nov-2023

---
+++ 1611-Minimum One Bit Operations to Make Integers Zero/code.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int minimumOneBitOperations(int n)
+    {
+        vector<int> f(31, 0);
+        f[0] = 1;
+        for (int i = 1; i <= 30; i++)
+        {
+            f[i] = 2 * f[i - 1] + 1;
+        }
+        int ans = 0, count = 0;
+        for (int i = 30; i >= 0; i--)
+        {
+            if ((1 << i) & n)
+            {
+                count++;
+                if (count % 2)
+                    ans += f[i];
+                else
+                    ans -= f[i];
+            }
+        }
+        return ans;
+    }
+};

fc73630c6bdfa6ccc60d6edf5954a8bbd7e8fe65
bhattShahab <aakashbhatt758@gmail.com>
2023-12-05 09:05:55 +0530

    Leetcode POTD 05-Dec-2023

---
+++ 1688-Count of Matches in Tournament/code.cpp
@@ -0,0 +1,37 @@
+// O(logn)--->>Time
+// O(1)---->space
+
+class Solution
+{
+public:
+    int numberOfMatches(int n)
+    {
+        int ans = 0;
+        while (n != 1)
+        {
+            if (n % 2)
+            {
+                ans += ((n - 1) / 2);
+                n = (n - 1) / 2 + 1;
+            }
+            else
+            {
+                ans += (n / 2);
+                n /= 2;
+            }
+        }
+        return ans;
+    }
+};
+
+// O(1)---->Time
+// O(1)---->Space
+
+class Solution
+{
+public:
+    int numberOfMatches(int n)
+    {
+        return n - 1;
+    }
+};
